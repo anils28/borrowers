10a166d30f237d37a1ac8710407a775cedd941f6
anils28 <28anils@gmail.com>
2014-09-29 19:33:45 +0530

    second commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('friends', function() { 
+  	this.route('new');
+  	this.route('show', { path: ':friend_id' });
+   });
+  
+  this.route('friends/show');
 });
 
 export default Router;
